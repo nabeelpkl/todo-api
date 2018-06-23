c450610379d2d7fa342928f7a8ea967bb80ed0ee
Nabeel K <nabeel.kottol@gmail.com>
2018-06-23 23:17:35 +0530

    Improving app config

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var { User } = require('./models/user');
 var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
